90135d08ea038c5c7bb1be633083fd9d04027a5a
kolgenpav <kolgenpav@gmail.com>
2023-03-19 19:20:57 +0200

    EmailChecker parameterized test with Test NG was added.

--- src/test/java/org/example/EmailCheckerTest.java
+++ src/test/java/org/example/EmailCheckerTest.java
@@ -1,20 +1,15 @@
 package org.example;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 class EmailCheckerTest {
 
     private EmailChecker emailChecker;
 
-    @BeforeEach
+    @BeforeClass
     void setUp() {
         emailChecker = new EmailChecker();
     }
@@ -22,20 +17,21 @@ class EmailCheckerTest {
     /**
      * Email address variants provider.
      */
-    private static Stream<Arguments> emailAddressProvider() {
-        return Stream.of(
-                Arguments.of("auser_1@gmail.com", false),
-                Arguments.of("Auser.1.1@gmail.com", false),
-                Arguments.of("1user@gmail.com", false),
-                Arguments.of("1zuser.@gmail.com", false),
-                Arguments.of("Zuser@com", false),
-                Arguments.of("user@gmail.c", false),
-                Arguments.of("zuser.@gmail.com", false),
-                Arguments.of("Zuser@com", false),
-                Arguments.of("user1@com", false),
-                Arguments.of("user@gmail.c", false),
-                Arguments.of("user@gmail.com", true)
-        );
+    @DataProvider
+    Object[][] emailAddressProvider() {
+        return new Object[][]{
+                {"auser_1@gmail.com", false},
+                {"Auser.1.1@gmail.com", false},
+                {"1user@gmail.com", false},
+                {"1zuser.@gmail.com", false},
+                {"Zuser@com", false},
+                {"user@gmail.c", false},
+                {"zuser.@gmail.com", false},
+                {"Zuser@com", false},
+                {"user1@com", false},
+                {"user@gmail.c", false},
+                {"user@gmail.com", true}
+        };
     }
 
     /**
@@ -44,10 +40,8 @@ class EmailCheckerTest {
      * @param email   address ti be verified
      * @param isValid verification result
      */
-    @DisplayName("Email address checking test")
-    @ParameterizedTest(name = "Email address {0} is valid: {1}")
-    @MethodSource("emailAddressProvider")
-    void checkEmail(String email, boolean isValid) {
-        assertTrue(emailChecker.checkEmail(email) == isValid);
+    @Test(dataProvider = "emailAddressProvider")
+    public void checkEmail(String email, boolean isValid) {
+       Assert.assertTrue(emailChecker.checkEmail(email) == isValid);
     }
 }
